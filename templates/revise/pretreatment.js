76456d5b10e11562338f115943b10e70be2bb91f
jinwooseok <jinus7949@naver.com>
2023-11-06 22:31:51 +0900

    fix : 전처리 저장 오류 처리

--- templates/revise/pretreatment.js
+++ templates/revise/pretreatment.js
@@ -1,10 +1,9 @@
 const $pretreatmentFileName = document.querySelector('#pretreatmentFileName');
 const $pretreatmentSave = document.querySelector('#pretreatmentSave');
-const excel_data = JSON.parse(document.getElementById("jsonObject").value);
 
 $pretreatmentSave.addEventListener('click', () => {
 	$.ajax({
-		url: `revise/${JSON.parse(localStorage.getItem("title_list"))}/preprocess/`,
+		url: `/revise/${JSON.parse(localStorage.getItem("fileTitle"))}/preprocess/`,
 		type:'post',
 		dataType: 'json',
 		headers: { 'X-CSRFToken': csrftoken },
@@ -13,7 +12,7 @@ $pretreatmentSave.addEventListener('click', () => {
 		},
 		success:function(response){
 				alert("완료되었습니다.");
-				window.location.href = "/fileList/";
+				window.location.href = "/file-list/";
 		},
 })
 })
