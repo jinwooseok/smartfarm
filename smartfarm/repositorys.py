6ce6362b9355a3d8a2918b59ad23697c56f185b2
jinwooseok <jinus7949@naver.com>
2023-11-06 20:53:17 +0900

    refactor : 모델에서 데이터를 호출하는 클래스 분리

--- smartfarm/repositorys.py
+++ smartfarm/repositorys.py
@@ -1,4 +1,9 @@
 from .models import File
-def findFileObjectByUserId(user_id):
+def findFileObjectListByUserId(user_id):
     return File.objects.filter(user_id=user_id) 
 
+def findFileObjectByUserIdFileTitle(user_id, file_title):
+    return File.objects.get(user_id=user_id, file_title=file_title)
+
+def findFileObjectListByUserIdFileTitle(user_id, file_title):
+    return File.objects.filter(user_id=user_id, file_title=file_title)
