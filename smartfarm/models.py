966e56fb6d8c3bf36191f5a3c7b3bc3ef05c4652
jinwooseok <jinus7949@naver.com>
2023-11-19 23:21:28 +0900

    refactor : models 사용하지 않는 의존성 제거

--- smartfarm/models.py
+++ smartfarm/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from users.models import User
-import os
-from config import settings
+
 # Create your models here.
 def user_media_path(instance, file_root):
     # 파일이 저장될 경로: media/file/<user_id>/<filename>
