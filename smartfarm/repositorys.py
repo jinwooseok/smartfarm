ba7ab42ae5a6dbe3f24c9f5a2c3337ae370dc84a
jinwooseok <jinus7949@naver.com>
2023-11-20 21:08:26 +0900

    refactor : 파일리스트 순서 반대로

--- smartfarm/repositorys.py
+++ smartfarm/repositorys.py
@@ -1,6 +1,6 @@
 from .models import File
 def findFileObjectListByUserId(user_id):
-    return File.objects.filter(user_id=user_id) 
+    return File.objects.filter(user_id=user_id).order_by('-created_at')
 
 def findFileObjectByUserIdFileTitle(user_id, file_title):
     return File.objects.get(user_id=user_id, file_title=file_title)
