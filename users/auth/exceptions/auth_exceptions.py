8a3dc991fc615804d791d48eb3e806369d42fc8c
jinwooseok <jinus7949@naver.com>
2024-01-17 13:17:34 +0900

    feat : 1001부터 >> 452부터

--- users/auth/exceptions/auth_exceptions.py
+++ users/auth/exceptions/auth_exceptions.py
@@ -3,25 +3,25 @@ from common.base_exception import CustomBaseException
 #회원가입
 class EmailDuplicatedException(CustomBaseException):
     def __init__(self):
-        self.status_code = 400
-        self.code = 1001
+        self.status_code = 452
+        self.code = 452
         self.detail = '이미 존재하는 이메일입니다.'
 
 class UserTelDuplicatedException(CustomBaseException):
     def __init__(self):
-        self.status_code = 400
-        self.code = 1002
+        self.status_code = 453
+        self.code = 453
         self.detail = '이미 존재하는 전화번호입니다.'
 
 #로그인
 class IdNotFoundException(CustomBaseException):
     def __init__(self):
-        self.status_code = 404
-        self.code = 1001
+        self.status_code = 452
+        self.code = 452
         self.detail = '존재하지 않는 이메일입니다.'
 
 class PasswordNotMatchedException(CustomBaseException):
     def __init__(self):
-        self.status_code = 400
-        self.code = 1002
+        self.status_code = 453
+        self.code = 453
         self.detail = '비밀번호가 일치하지 않습니다.'
