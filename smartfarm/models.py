08ee9cb028da210612622112e19d3217a7a5b520
jinwooseok <jinus7949@naver.com>
2024-01-17 13:13:17 +0900

    feat : model변경

--- smartfarm/models.py
+++ smartfarm/models.py
@@ -7,11 +7,36 @@ def user_media_path(instance, file_root):
     return 'file/{0}/{1}'.format(instance.user_id.id, file_root)
 
 class File(models.Model):
-    file_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE,default=000000)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=000000)
     file_type = models.CharField(max_length=200,blank=True)
     file_title = models.CharField(max_length=200)
     file_root = models.FileField(upload_to=user_media_path,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+class Status(models.Model):
+    file = models.ForeignKey(File,on_delete=models.CASCADE,default=000000)
+    status_id = models.IntegerChoices('status_id','0 1 2 3 4 5 6 7 8 9')
+    status_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class StatusCode(models.Model):
+    status_name = models.CharField(max_length=200)
+
+class Feature(models.Model):
+    file = models.ForeignKey(File,on_delete=models.CASCADE,default=000000)
+    feature_order = models.IntegerField()
+    feature_name = models.CharField(max_length=200)
+    feature_type = models.CharField(max_length=200)
+    feature_importance = models.FloatField()
+    feature_selected = models.BooleanField(default=False)
+
+class LearnedModel(models.Model):
+    file = models.ForeignKey(File,on_delete=models.CASCADE,default=000000)
+    model_name = models.CharField(max_length=200)
+    model_type = models.CharField(max_length=200)
+    model_score = models.FloatField()
+    model_params = models.CharField(max_length=200)
+    model_status = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
