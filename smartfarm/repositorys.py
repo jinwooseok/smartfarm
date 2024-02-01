07ac22cbd0ec6e0797ee145edc3005717961e134
jinwooseok <jinus7949@naver.com>
2024-02-01 12:52:05 +0900

    chore : 안쓰는 파일 제거

--- smartfarm/repositorys.py
+++
@@ -1,9 +0,0 @@
-from .models import File
-def findFileObjectListByUserId(user_id):
-    return File.objects.filter(user_id=user_id).order_by('-created_at')
-
-def findFileObjectByUserIdFileTitle(user_id, file_title):
-    return File.objects.get(user_id=user_id, file_title=file_title)
-
-def findFileObjectListByUserIdFileTitle(user_id, file_title):
-    return File.objects.filter(user_id=user_id, file_title=file_title)
