e8564ed837114cec3595318caf864e1abcc8db8a
jinwooseok <jinus7949@naver.com>
2024-02-05 10:35:51 +0900

    refactor : 유저부분 서비스 추가

---
+++ users/auth/service/auth_user_service.py
@@ -0,0 +1,27 @@
+from ..exceptions.auth_exceptions import IdNotFoundException, PasswordNotMatchedException
+from ...models import User
+from argon2 import PasswordHasher
+from ..repositorys import get_user_by_email
+from argon2.exceptions import VerifyMismatchError
+
+class AuthUserService:
+    def __init__(self, user_id, user_pw):
+        self.user_id = user_id
+        self.user_pw = user_pw
+    
+    @classmethod
+    def from_serializer(cls, serializer):
+        user_id = serializer.validated_data['email']
+        user_pw = serializer.validated_data['password']
+        return cls(user_id, user_pw)
+    
+    def execute(self):
+        user = get_user_by_email(self.user_id)
+        self.is_correct_password(self.user_pw, user.user_pw)
+        return user
+
+    def is_correct_password(login_pw, user_pw):
+        try:
+            return PasswordHasher().verify(login_pw.encode(), user_pw.encode())
+        except VerifyMismatchError:    
+            raise PasswordNotMatchedException()
---
+++ users/auth/service/save_user_service.py
@@ -0,0 +1,51 @@
+from ..exceptions.auth_exceptions import EmailDuplicatedException, UserTelDuplicatedException
+from argon2 import PasswordHasher
+from ...models import User
+from ..repositorys import exist_user_by_email, exist_user_by_tel
+
+class SaveUserService:
+    def __init__(self, user_id, user_pw, user_name, user_job, user_tel):
+        self.user_id = user_id
+        self.user_pw = user_pw
+        self.user_name = user_name
+        self.user_job = user_job
+        self.user_tel = user_tel
+    
+    @classmethod
+    def from_serializer(cls, serializer):
+        user_id = serializer.validated_data['user_id']
+        user_pw = serializer.validated_data['user_pw']
+        user_name = serializer.validated_data['user_name']
+        user_job = serializer.validated_data['user_job']
+        user_tel = serializer.validated_data['user_tel']
+        return cls(user_id, user_pw, user_name, user_job, user_tel)
+    
+    def execute(self):
+        if self.is_duplicated_email(self.user_id):
+            raise EmailDuplicatedException()
+        #비밀번호 암호화
+        self.user_pw = PasswordHasher().hash(self.user_pw)
+        #전화번호 합체
+        self.user_tel = ''.join(self.user_tel)
+        #전화번호 검증
+        if self.is_duplicated_user_tel(self.user_tel):
+            raise UserTelDuplicatedException()
+        #DB에 저장
+        user_form = self.user_form()
+        user_form.save()
+    
+    @staticmethod
+    def is_duplicated_user_tel(user_tel):
+        user_tel = ''.join(user_tel)
+        return exist_user_by_tel(user_tel)
+    
+    @staticmethod
+    def is_duplicated_email(email):
+        return exist_user_by_email(email)
+    
+    def user_form(self):
+        return User(user_id=self.user_id
+                    , user_pw=self.user_pw
+                    , user_name=self.user_name
+                    , user_job=self.user_job
+                    , user_tel=self.user_tel)
