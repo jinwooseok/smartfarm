836dba8477f1381a4c626512eae5118f7a3c5fab
jinwooseok <jinus7949@naver.com>
2024-02-05 10:36:07 +0900

    feat : 데이터 추출 부분 추가

---
+++ users/auth/repositorys.py
@@ -0,0 +1,13 @@
+from ..models import User
+from .exceptions.auth_exceptions import IdNotFoundException
+def exist_user_by_email(email):
+    return User.objects.filter(user_id=email).exists()
+
+def exist_user_by_tel(tel):
+    return User.objects.filter(user_tel=tel).exists()
+
+def get_user_by_email(email):
+    try:
+        return User.objects.get(user_id=email)
+    except User.DoesNotExist:
+        raise IdNotFoundException()
