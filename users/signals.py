ccb9d6ed0814e85416ad367c32fddf8cb4226192
jinwooseok <jinus7949@naver.com>
2024-02-14 11:51:22 +0900

    feat : 유저 생성 시 디렉토리 자동 생성

---
+++ users/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import pre_delete, post_save
+from django.dispatch import receiver
+from .models import User
+import os
+from django.conf import settings
+@receiver(post_save, sender=User)
+def create_directory(sender, instance, **kwargs):
+    # 사용자가 생성될 때마다 사용자의 디렉토리를 생성
+    os.makedirs(os.path.join(settings.MEDIA_ROOT, str(instance.id)))
+    for dir in ['file', 'temp', 'model']:
+        os.makedirs(os.path.join(settings.MEDIA_ROOT, str(instance.id), dir)) 
