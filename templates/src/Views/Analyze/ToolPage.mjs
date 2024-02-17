67343a8db3dfef9354240901fe1fb4570b8fce73
Seungmani <dkelcncjs@naver.com>
2024-02-18 00:18:14 +0900

    [fix] 모델 생섬 및 시차 변수 페이지 완성

--- templates/src/Views/Analyze/ToolPage.mjs
+++ templates/src/Views/Analyze/ToolPage.mjs
@@ -1,3 +1,6 @@
+import API from "/templates/src/Utils/API.mjs";
+import responseMessage from "/templates/src/Constant/responseMessage.mjs";
+
 class ToolPage {
 	#varList;
 	#featureNameList = [];
@@ -25,26 +28,30 @@ class ToolPage {
 					${switchHtml}
 				</div>
 			</div>
+
+			<div class="timeDIV">
+				<fieldset class="timeDiffDIV">
+					<legend>시차 변수 선택</legend>
+					<div class="timeDiffUseDIV" id="timeDiffUseDIV">
+						<input type="radio" name="time" id="not" value="not" checked><label for="not">사용 x</label>
+						<input type="radio" name="time" id="use" value="use"><label for="use">사용</label>
+					</div>
+					</select>
+				</fieldset>
+
+				<button class="timeDiffBtn" id="timeDiffCreate" disabled>시차변수 생성</button>
+			</div>
+
 			<div class="fieldsetDIV">
 				<fieldset class="yDIV">
 					<legend>y 값</legend>
 					<select name="yValue" id="yValue" class="yValue">
-					
-					</select>
-				</fieldset>
-
-				<fieldset class="typeDIV">
-					<legend>분석 종류</legend>
-					<select name="technique" id="technique" class="technique">
-						<option value="linear">선형회귀분석</option>
-						<option value="lstm">로지스틱회귀분석</option>
-						<option value="rf">랜텀포레스트</option>
 					</select>
 				</fieldset>
 
 				<fieldset class="trainSizeDIV">
 					<legend>trainSize</legend>
-					<input type="text" name="trainSize" class="trainSize" id="trainSize" placeholder="0~1">
+					<input type="text" name="trainSize" class="trainSize" id="trainSize" value="0.7">
 				</fieldset>
 
 				<fieldset class="nameDIV">
@@ -52,14 +59,49 @@ class ToolPage {
 					<input type="text" name="modelName" class="modelName" id="modelName" placeholder="모델 이름">
 				</fieldset>
 
-				<button class="create" id="create">생성하기</button>
+				<button class="create" id="create">모델 생성</button>
 			</div>
 		</div>
 		`
 	}
 
-	async setVarList(list) {
-		this.#varList = list;
+	onClickTimeDIff(id) {
+		if (id === "use") {
+			return `
+			<fieldset class="timeDiffDIV">
+				<legend>시차 변수 선택</legend>
+				<div class="timeDiffUseDIV" id="timeDiffUseDIV">
+					<input type="radio" name="time" id="not" value="not"><label for="not">사용 x</label>
+					<input type="radio" name="time" id="use" value="use" checked><label for="use">사용</label>
+				</div>
+				</select>
+			</fieldset>
+
+			<fieldset class="countDIV">
+				<legend>count</legend>
+				<input type="number" name="count" class="count" id="count" placeholder="count">
+			</fieldset>
+
+			<fieldset class="windowSizeDIV">
+				<legend>windowSize</legend>
+				<input type="number" name="windowSize" class="windowSize" id="windowSize" placeholder="windowSize">
+			</fieldset>
+
+			<button class="timeDiffBtn" id="timeDiffCreate">시차변수 생성</button>
+			`
+		}
+		return `
+		<fieldset class="timeDiffDIV">
+			<legend>시차 변수 선택</legend>
+			<div class="timeDiffUseDIV" id="timeDiffUseDIV">
+				<input type="radio" name="time" id="not" value="not" checked><label for="not">사용 x</label>
+				<input type="radio" name="time" id="use" value="use" ><label for="use">사용</label>
+			</div>
+			</select>
+		</fieldset>
+
+		<button class="timeDiffBtn" id="timeDiffCreate" disabled>시차변수 생성</button>
+		`
 	}
 
 	#makeVarListDIV() {
@@ -95,13 +137,28 @@ class ToolPage {
 				checkedVar.push($$switch[i].parentElement.parentElement.childNodes[1].innerText);
 			}
 		}
-
 		return checkedVar;
 	}
 
-	// 파일 정보 받기
+	async setVarList(fileName) {
+		const response = await API(`/files/${fileName}/data/feature/`, "get");
+		const status = response.status || response;
+		if (response.status === "success") {
+			return responseMessage[status] === "success" ? this.#varList = response.data : alert(responseMessage[status]);
+		}
+	}
 
-	// API 연동
+	async postModelData(data) {
+		const response = await API(`/analytics/${fileName}/model/`, "post", data);
+    const status = response.status || response;
+    alert(responseMessage[status]);
+	}
+
+	async postTimeDiffData(data) {
+		const response = await API(`/files/${timeDiffName}/data/timeseries/`, "post", data);
+		const status = response.status || response;
+		responseMessage[status] === "success" ? location.replace("/file-list/") : alert(responseMessage[status]);
+	} 
 
 	getFeatureNameList() {
 		return this.#featureNameList;
--- templates/src/Views/Analyze/analyze.mjs
+++ templates/src/Views/Analyze/analyze.mjs
@@ -1,49 +1,16 @@
 import Logout from "/templates/src/Utils/Logout.mjs";
 import Loading from "/templates/src/Utils/Loading.mjs";
-import API from "/templates/src/Utils/API.mjs";
-import { getFileNameList, setFileList } from "/templates/src/Utils/fileNameList.mjs";
+import { setFileList } from "/templates/src/Utils/fileNameList.mjs";
 
 import ToolPage from "./ToolPage.mjs";
 
 const $logoutBtn = document.querySelector("#logoutBtn");
 $logoutBtn.addEventListener("click", Logout);
 
-const $fileListSelectBox = document.querySelector("#fileListSelectBox");
-const $uploadDialog = document.querySelector("#uploadDialog");
-
 ////////////////////// 파일 변경
 const fileName = JSON.parse(localStorage.getItem("fileTitle"));
 
-const moveSelectedFileTitle = () => {
-  const selectedFileTitle =  $fileListSelectBox.options[$fileListSelectBox.selectedIndex].value;
-  localStorage.setItem("fileTitle", JSON.stringify(selectedFileTitle));
-  location.href = `/analytics/${selectedFileTitle}/`;
-};
-
-$fileListSelectBox.addEventListener("change", moveSelectedFileTitle);
-
-const fileList = await getFileNameList();
-setFileList($fileListSelectBox, fileList, fileName);
-
-const getVarList = async () => {
-  const response = await API(`/files/${fileName}/data/feature/`, "get");
-  const status = response.status || response;
-  if (response.status === "success") {
-    return responseMessage[status] === "success" ? await ToolPage.setVarList(response.data) : alert(responseMessage[status]);
-  }
-}
-
-// 파일 분석 창으로 이동
-const handleClickMovePage = async () => {
-  await getVarList();
-  const $workDIV = document.querySelector(".workDIV");
-  $workDIV.innerHTML = ToolPage.templates();
-
-  const $yValue = document.querySelector(".yValue");
-  setFileList($yValue, ToolPage.getFeatureNameList()); 
-}
-
-const clickEvent = async (event, id, targetClass) => {
+const clickEvent = async (event, id, disabled) => {
   if (id === "create") {
     const data = {
       modelName: document.querySelector(".modelName").value,
@@ -51,38 +18,43 @@ const clickEvent = async (event, id, targetClass) => {
       trainSize: document.querySelector(".trainSize").value,
       yValue: document.querySelector(".yValue").options[document.querySelector(".yValue").selectedIndex]?.value,
       xValue: JSON.stringify(ToolPage.setCheckedVarList()),
-    }
-    const response = await API(`/analytics/${fileName}/model/`, "post", data);
-    const status = response.status || response;
-    alert(responseMessage[status]);
-   }
+    };
+    await ToolPage.postModelData(data);
+  }
 
   if (
-    id === "fileUpload" ||
-    id === "movePage"
-  ) {
-    await handleClickMovePage();
+    id === "use" ||
+    id === "not"
+    ) {
+    const $timeDIV = document.querySelector(".timeDIV");
+    $timeDIV.innerHTML = ToolPage.onClickTimeDIff(id);
   }
 
-  if (id === "fileUploadBtn") {
-    $uploadDialog.style.display = "flex";
-    $uploadDialog.showModal();
+  if (id === "timeDiffCreate") {
+    const data = {
+      feature: JSON.stringify(ToolPage.setCheckedVarList()),
+      windowSize: document.querySelector("#windowSize").value,
+      count: document.querySelector("#count").value,
+      newFileName: document.querySelector(".modelName").value,
+    };
+    await ToolPage.postTimeDiffData(data);
   }
-
-  if (id === "closeUploadPopup") {
-    $uploadDialog.style.display = "none";
-    $uploadDialog.close();
-  }
-
 }
-
 
 window.addEventListener("click", (event) => {
 	const targetId = event.target.id;
-	const targetClass= event.target.classList;
+	const disabled= event.target.disabled;
 
 	if (targetId !== "") {
-		clickEvent(event, targetId, targetClass);
+		clickEvent(event, targetId, disabled);
 	}
 })
 
+window.onload = async () => {
+  await ToolPage.setVarList(fileName);
+  const $workDIV = document.querySelector(".workDIV");
+  $workDIV.innerHTML = ToolPage.templates();
+
+  const $yValue = document.querySelector(".yValue");
+  setFileList($yValue, ToolPage.getFeatureNameList()); 
+}
