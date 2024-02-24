06b9ded3a4b41f3945edbe95bc84cf39f8d548f5
jinwooseok <jinus7949@naver.com>
2024-02-24 21:37:39 +0900

    feat : 모델 삭제 스케줄러

--- smartfarm/scheduler.py
+++ smartfarm/scheduler.py
@@ -1,26 +1,26 @@
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from apscheduler.triggers.cron import CronTrigger
-# import logging
-# from .models import Temp, LearnedModel
-# from django.utils import timezone
-# logger = logging.getLogger(__name__)
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import logging
+from .models import Temp, LearnedModel
+from django.utils import timezone
+logger = logging.getLogger('django')
 
-# def temp_model_delete():
-#     delete_time = timezone.now()-timezone.timedelta(days=1)
-#     Temp.object.filter(created_at__lt=delete_time).delete()
-#     LearnedModel.object.filter(created_at__lt=delete_time).delete()    
-# def start():
-#     scheduler=BackgroundScheduler()
-#     scheduler.add_job(
-#         temp_model_delete,
-#         trigger=CronTrigger(hour=0, minute=0),
-#         id="delete",
-#         misfire_grace_time=300,
-#         replace_existing=True,
-#     )
-#     try:
-#         logger.info("Starting scheduler...")
-#         scheduler.start()
-#     except KeyboardInterrupt:
-#         logger.info("Stopping scheduler...")
-#         scheduler.shutdown()
+def temp_model_delete():
+    delete_time = timezone.now()-timezone.timedelta(days=1)
+    Temp.object.filter(created_at__lt=delete_time).delete()
+    LearnedModel.object.filter(created_at__lt=delete_time).delete()    
+def start():
+    scheduler=BackgroundScheduler()
+    scheduler.add_job(
+        temp_model_delete,
+        trigger=CronTrigger(hour=0, minute=0),
+        id="delete",
+        misfire_grace_time=300,
+        replace_existing=True,
+    )
+    try:
+        logger.info("Starting scheduler...")
+        scheduler.start()
+    except KeyboardInterrupt:
+        logger.info("Stopping scheduler...")
+        scheduler.shutdown()
