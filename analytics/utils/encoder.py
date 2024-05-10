6856bf75860eaa6dd2bd5713369fc2cd64dbc2b0
jinwooseok <jinus7949@naver.com>
2024-05-10 21:24:01 +0900

    docs : encoding docs

--- analytics/utils/encoder.py
+++ analytics/utils/encoder.py
@@ -1,18 +1,32 @@
+"""
+라벨인코더, 원핫인코더를 사용하여 범주형 데이터를 수치형 데이터로 변환하는 클래스
+"""
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import pandas as pd
 class Encoder:
-    def __init__(self):
-        pass
+    """
+    설명
+    - 라벨인코더, 원핫인코더를 사용하여 범주형 데이터를 수치형 데이터로 변환하는 클래스
+    """
     @staticmethod
     def encode(categorical_data, method):
+        """
+        설명
+        - 범주형 데이터를 수치형 데이터로 변환
+        
+        매개변수
+        - categorical_data (pd.DataFrame): 범주형 데이터
+        - method (str): 인코딩 방법 (label, onehot)
+        
+        반환값
+        - 인코딩된 데이터
+        """
         if method == "label":
             le = LabelEncoder()
         elif method == "onehot":
             le = OneHotEncoder()
-        # 라벨인코더 선언 및 Fitting
-        le = LabelEncoder()
         categorical_data = pd.DataFrame(categorical_data)
-        encoded_data = categorical_data.apply(lambda col: le.fit_transform(col))
+        encoded_data = le.fit_transform(categorical_data)
         if categorical_data.shape[1] == 1:
-            encoded_data = encoded_data.iloc[:, 0]
+            encoded_data = encoded_data[:, 0]
         return encoded_data
