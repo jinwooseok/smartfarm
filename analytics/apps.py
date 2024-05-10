b3c81cf8f3ae66ffbef4111fd6aafd90eb9fd5cb
jinwooseok <jinus7949@naver.com>
2024-05-10 21:45:33 +0900

    docs : analytics apps, signals

--- analytics/apps.py
+++ analytics/apps.py
@@ -1,10 +1,17 @@
 from django.apps import AppConfig
 
-
 class AnalyticsConfig(AppConfig):
+    """
+    AnalyticsConfig 클래스는 앱의 설정을 정의하는 클래스입니다.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analytics'
     def ready(self):
+        """
+        - ready 메서드는 앱이 실행될 때 호출되는 메서드입니다. 서버 실행 시 호출됨.
+        - signals 모듈을 import하여 모델 삭제 전에 파일을 삭제하도록 합니다.
+        - scheduler를 import하여 실행합니다.
+        """
         from analytics.utils import signals
         from config import scheduler
         scheduler.start()
--- analytics/utils/signals.py
+++ analytics/utils/signals.py
@@ -1,3 +1,6 @@
+"""
+DB에 삭제 신호가 오는 경우, 파일 시스템에 저장된 모델을 자동으로 삭제
+"""
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from analytics.models import LearnedModel
@@ -5,5 +8,11 @@ from file.utils.utils import search_file_absolute_path
 from file.utils.signals import delete_local_file
 @receiver(pre_delete, sender=LearnedModel)
 def delete_model(sender, instance, **kwargs):
+    """
+    설명
+    - 모델이 삭제되기 전에 호출되는 함수.
+    - DB에서 모델이 삭제되면 모델 파일을 삭제한다.
+    - receiver 데코레이터를 사용하여 LearnedModel로부터 신호를 받는다. 모델이 삭제되기 전에 호출되도록 설정
+    """
     delete_local_file(search_file_absolute_path(instance.model_root))
     delete_local_file(search_file_absolute_path(instance.model_meta_root))
